405df8eca5f867b74fb5cf1450741619cd2bf46d
cavidan <118810437+cavidanmercan@users.noreply.github.com>
2023-06-01 10:04:35 +0300

    update

---
+++ force-app/main/default/lwc/practice15/practice15.js
@@ -0,0 +1,9 @@
+import { LightningElement } from "lwc";
+
+export default class Practice15 extends LightningElement {
+}
+
+
+
+
+//js de birşey yapmadık
---
+++ force-app/main/default/lwc/trailblazerRankMentor/trailblazerRankMentor.js
@@ -0,0 +1,19 @@
+import { LightningElement } from "lwc";
+export default class TrailblazerRankMentor extends LightningElement {
+
+badges=0;
+points=0;
+
+handleBadgeChange(event){
+    this.badges=event.target.value;
+}
+
+handlePointChange(event){
+    this.points=event.target.value;
+}
+
+get isRanger(){
+    return this.badges>100 && this.points>50000;
+}
+
+}
